604599a0cf9ee30c0679fb9917b5de806ba98505
Andrew Mcdonald <drew887121@gmail.com>
2022-04-29 11:02:40 -0400

    Addresses more PR comments

--- alpaca/common/enums.py
+++ alpaca/common/enums.py
@@ -12,6 +12,23 @@ class BaseURL(str, Enum):
     MARKET_DATA_LIVE = "wss://stream.data.alpaca.markets"
 
 
+class PaginationType(str, Enum):
+    """
+    An enum for choosing what type of pagination of results you'd like for BrokerClient functions that support
+    pagination.
+
+    Attributes:
+        NONE: Requests that we perform no pagination of results and just return the single response the API gave us.
+        FULL: Requests that we perform all the pagination and return just a single List/dict/etc containing all the
+          results. This is the default for most functions.
+        ITERATOR: Requests that we return an Iterator that yields one "page" of results at a time
+    """
+
+    NONE = "none"
+    FULL = "full"
+    ITERATOR = "iterator"
+
+
 class Sort(str, Enum):
     ASC = "asc"
     DESC = "desc"

132a676236e81a9bffcaf8daad77ae3173a8c1ad
haxdds <haxdds@gmail.com>
2022-05-23 09:48:13 -0400

    adds tests for submit_order

---
+++ tests/trading/trading_client/test_order_routes.py
@@ -0,0 +1,68 @@
+from alpaca.trading.client import TradingClient
+from alpaca.common.enums import OrderSide, OrderStatus, OrderType, TimeInForce
+from alpaca.trading.models import OrderCreationRequest
+from alpaca.common.enums import BaseURL
+
+import pytest
+
+
+@pytest.fixture
+def trading_client():
+    client = TradingClient("key-id", "secret-key")
+    return client
+
+
+def test_submit_order(reqmock, trading_client):
+
+    reqmock.post(
+        f"{BaseURL.TRADING_PAPER}/v2/orders",
+        text="""
+        {
+          "id": "61e69015-8549-4bfd-b9c3-01e75843f47d",
+          "client_order_id": "eb9e2aaa-f71a-4f51-b5b4-52a6c565dad4",
+          "created_at": "2021-03-16T18:38:01.942282Z",
+          "updated_at": "2021-03-16T18:38:01.942282Z",
+          "submitted_at": "2021-03-16T18:38:01.937734Z",
+          "filled_at": null,
+          "expired_at": null,
+          "canceled_at": null,
+          "failed_at": null,
+          "replaced_at": null,
+          "replaced_by": null,
+          "replaces": null,
+          "asset_id": "904837e3-3b76-47ec-b432-046db621571b",
+          "symbol": "AAPL`",
+          "asset_class": "us_equity",
+          "notional": null,
+          "qty": 1,
+          "filled_qty": "0",
+          "filled_avg_price": null,
+          "order_class": "simple",
+          "order_type": "market",
+          "type": "market",
+          "side": "buy",
+          "time_in_force": "day",
+          "limit_price": null,
+          "stop_price": null,
+          "status": "accepted",
+          "extended_hours": false,
+          "legs": null,
+          "trail_percent": null,
+          "trail_price": null,
+          "hwm": null,
+          "commission": 1.25
+        }
+        """,
+    )
+
+    mo = OrderCreationRequest(
+        symbol="SPY",
+        side=OrderSide.BUY,
+        type=OrderType.MARKET,
+        time_in_force=TimeInForce.DAY,
+        qty=1,
+    )
+
+    mo_response = trading_client.submit_order(mo)
+
+    assert mo_response.status == OrderStatus.ACCEPTED

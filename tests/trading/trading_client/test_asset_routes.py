9abae926049207b1540e6ac4bb434bc0811fdc70
haxdds <haxdds@gmail.com>
2022-05-31 09:47:45 -0400

    added trading_client:asset routes

---
+++ tests/trading/trading_client/test_asset_routes.py
@@ -0,0 +1,72 @@
+"""
+Contains tests for Trading API's asset routes.
+"""
+
+from typing import List
+from alpaca.common.enums import BaseURL, AssetStatus
+from alpaca.common.models import Asset
+from alpaca.trading.client import TradingClient
+from alpaca.trading.models import GetAssetsRequest
+
+
+def test_get_all_assets(reqmock, trading_client: TradingClient):
+    reqmock.get(
+        f"{BaseURL.TRADING_PAPER}/v2/assets",
+        text="""
+        [
+            {
+              "id": "904837e3-3b76-47ec-b432-046db621571b",
+              "class": "us_equity",
+              "exchange": "NASDAQ",
+              "symbol": "AAPL",
+              "name": "Apple Inc. Common Stock",
+              "status": "active",
+              "tradable": true,
+              "marginable": true,
+              "shortable": true,
+              "easy_to_borrow": true,
+              "fractionable": true,
+              "last_close_pct_change": "string",
+              "last_price": "string"
+            }
+      ]
+      """,
+    )
+
+    assets = trading_client.get_all_assets(GetAssetsRequest(status=AssetStatus.ACTIVE))
+
+    assert reqmock.called_once
+    assert isinstance(assets, List)
+    assert len(assets) == 1
+    assert isinstance(assets[0], Asset)
+
+
+def test_get_asset(reqmock, trading_client: TradingClient):
+    symbol = "AAPL"
+
+    reqmock.get(
+        f"{BaseURL.TRADING_PAPER}/v2/assets/{symbol}",
+        text="""
+            {
+              "id": "904837e3-3b76-47ec-b432-046db621571b",
+              "class": "us_equity",
+              "exchange": "NASDAQ",
+              "symbol": "AAPL",
+              "name": "Apple Inc. Common Stock",
+              "status": "active",
+              "tradable": true,
+              "marginable": true,
+              "shortable": true,
+              "easy_to_borrow": true,
+              "fractionable": true,
+              "last_close_pct_change": "string",
+              "last_price": "string"
+            }
+            """,
+    )
+
+    asset = trading_client.get_asset(symbol)
+
+    assert reqmock.called_once
+    assert isinstance(asset, Asset)
+    assert asset.symbol == symbol

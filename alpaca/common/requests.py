65f18639329cddc09e971ff1b7f066fc452ff473
Chihiro Hio <chihiro.hio@alpaca.markets>
2023-11-08 17:09:34 +0900

    fix: fix missing import

--- alpaca/common/requests.py
+++ alpaca/common/requests.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from typing import Any
 from uuid import UUID
 
@@ -49,7 +49,7 @@ class NonEmptyRequest(BaseModel):
                 # if the datetime is naive, assume it's UTC
                 # https://docs.python.org/3/library/datetime.html#determining-if-an-object-is-aware-or-naive
                 if val.tzinfo is None or val.tzinfo.utcoffset(val) is None:
-                    val = val.replace(tzinfo=datetime.timezone.utc)
+                    val = val.replace(tzinfo=timezone.utc)
                 return val.isoformat()
 
             return val

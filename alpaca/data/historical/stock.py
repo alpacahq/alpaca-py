3726c1f681825b6330bbe923373db4cd61435e6e
Chihiro Hio <chihiro.hio@alpaca.markets>
2023-12-15 13:59:33 +0900

    fix: handle empty bars

--- alpaca/data/historical/stock.py
+++ alpaca/data/historical/stock.py
@@ -98,6 +98,9 @@ class StockHistoricalDataClient(RESTClient):
 
         if self._use_raw_data:
             return raw_bars
+    
+        if raw_bars is None:
+            return None
 
         return BarSet(raw_bars)
 
@@ -229,6 +232,9 @@ class StockHistoricalDataClient(RESTClient):
 
         if self._use_raw_data:
             return raw_latest_bars
+
+        if raw_latest_bars is None:
+            return {}
 
         return parse_obj_as_symbol_dict(Bar, raw_latest_bars)
 

eb86f4abe1dd44c924a16e7e4cbae514d6aaf1e4
Chihiro Hio <chihiro.hio@alpaca.markets>
2023-12-21 17:35:38 +0900

    fix: black and remove APIAuthError

--- alpaca/common/exceptions.py
+++ alpaca/common/exceptions.py
@@ -21,52 +21,9 @@ class APIError(Exception):
                 detailed_error["url"] = http_error.request.url
             # add tips for auth key error
             if detailed_error["status_code"] in [401, 403]:
-                detailed_error["tips"] = "please check your API key and environment (paper/sandbox/live)"
-
-        self._error = error
-        self._http_error = http_error
-        super().__init__(detailed_error)
-
-    @property
-    def code(self):
-        error = json.loads(self._error)
-        return error["code"]
-
-    @property
-    def status_code(self):
-        http_error = self._http_error
-        if http_error is not None and hasattr(http_error, "response"):
-            return http_error.response.status_code
-
-    @property
-    def request(self):
-        if self._http_error is not None:
-            return self._http_error.request
-
-    @property
-    def response(self):
-        if self._http_error is not None:
-            return self._http_error.response
-
-
-class APIAuthError(Exception):
-    """
-    Represent API Auth related error.
-    """
-
-    def __init__(self, error, http_error=None):
-        detailed_error = {
-            "message": error,
-            "tips": "please check your API key and environment (paper/sandbox/live)."
-        }
-
-        if http_error is not None:
-            if hasattr(http_error, "response"):
-                detailed_error["status_code"] = http_error.response.status_code
-                detailed_error["reason"] = http_error.response.reason
-            if hasattr(http_error, "request"):
-                detailed_error["method"] = http_error.request.method
-                detailed_error["url"] = http_error.request.url
+                detailed_error[
+                    "tips"
+                ] = "please check your API key and environment (paper/sandbox/live)"
 
         self._error = error
         self._http_error = http_error

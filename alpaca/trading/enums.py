26ed443e82c5d7adbc304f96c977054ad9587712
Chihiro Hio <chihiro.hio@alpaca.markets>
2024-01-24 14:31:18 +0900

    chore: adjust docstring format

--- alpaca/trading/enums.py
+++ alpaca/trading/enums.py
@@ -90,9 +90,9 @@ class OrderClass(str, Enum):
 
     The order classes supported by Alpaca vary based on the order's security type.
     The following provides a comprehensive breakdown of the supported order classes for each category:
-         - Equity trading: simple (or ""), oco, oto, bracket.
-         - Options trading: simple (or "").
-         - Crypto trading: simple (or "").
+    - Equity trading: simple (or ""), oco, oto, bracket.
+    - Options trading: simple (or "").
+    - Crypto trading: simple (or "").
     """
 
     SIMPLE = "simple"
@@ -107,9 +107,9 @@ class OrderType(str, Enum):
 
     The order types supported by Alpaca vary based on the order's security type.
     The following provides a comprehensive breakdown of the supported order types for each category:
-         - Equity trading: market, limit, stop, stop_limit, trailing_stop.
-         - Options trading: market, limit, stop, stop_limit.
-         - Crypto trading: market, limit, stop_limit.
+    - Equity trading: market, limit, stop, stop_limit, trailing_stop.
+    - Options trading: market, limit, stop, stop_limit.
+    - Crypto trading: market, limit, stop_limit.
     """
 
     MARKET = "market"
@@ -211,22 +211,16 @@ class TimeInForce(str, Enum):
     Represents the various time in force options for an Order.
 
     The Time-In-Force values supported by Alpaca vary based on the order's security type. Here is a breakdown of the supported TIFs for each specific security type:
-        - Equity trading: day, gtc, opg, cls, ioc, fok.
-        - Options trading: day.
-        - Crypto trading: gtc, ioc.
+    - Equity trading: day, gtc, opg, cls, ioc, fok.
+    - Options trading: day.
+    - Crypto trading: gtc, ioc.
     Below are the descriptions of each TIF:
-        - day:
-          A day order is eligible for execution only on the day it is live. By default, the order is only valid during Regular Trading Hours (9:30am - 4:00pm ET). If unfilled after the closing auction, it is automatically canceled. If submitted after the close, it is queued and submitted the following trading day. However, if marked as eligible for extended hours, the order can also execute during supported extended hours.
-        - gtc:
-          The order is good until canceled. Non-marketable GTC limit orders are subject to price adjustments to offset corporate actions affecting the issue. We do not currently support Do Not Reduce(DNR) orders to opt out of such price adjustments.
-        - opg:
-          Use this TIF with a market/limit order type to submit “market on open” (MOO) and “limit on open” (LOO) orders. This order is eligible to execute only in the market opening auction. Any unfilled orders after the open will be cancelled. OPG orders submitted after 9:28am but before 7:00pm ET will be rejected. OPG orders submitted after 7:00pm will be queued and routed to the following day’s opening auction. On open/on close orders are routed to the primary exchange. Such orders do not necessarily execute exactly at 9:30am / 4:00pm ET but execute per the exchange’s auction rules.
-        - cls:
-          Use this TIF with a market/limit order type to submit “market on close” (MOC) and “limit on close” (LOC) orders. This order is eligible to execute only in the market closing auction. Any unfilled orders after the close will be cancelled. CLS orders submitted after 3:50pm but before 7:00pm ET will be rejected. CLS orders submitted after 7:00pm will be queued and routed to the following day’s closing auction. Only available with API v2.
-        - ioc:
-          An Immediate Or Cancel (IOC) order requires all or part of the order to be executed immediately. Any unfilled portion of the order is canceled. Only available with API v2. Most market makers who receive IOC orders will attempt to fill the order on a principal basis only, and cancel any unfilled balance. On occasion, this can result in the entire order being cancelled if the market maker does not have any existing inventory of the security in question.
-        - fok:
-          A Fill or Kill (FOK) order is only executed if the entire order quantity can be filled, otherwise the order is canceled. Only available with API v2.
+    - day: A day order is eligible for execution only on the day it is live. By default, the order is only valid during Regular Trading Hours (9:30am - 4:00pm ET). If unfilled after the closing auction, it is automatically canceled. If submitted after the close, it is queued and submitted the following trading day. However, if marked as eligible for extended hours, the order can also execute during supported extended hours.
+    - gtc: The order is good until canceled. Non-marketable GTC limit orders are subject to price adjustments to offset corporate actions affecting the issue. We do not currently support Do Not Reduce(DNR) orders to opt out of such price adjustments.
+    - opg: Use this TIF with a market/limit order type to submit “market on open” (MOO) and “limit on open” (LOO) orders. This order is eligible to execute only in the market opening auction. Any unfilled orders after the open will be cancelled. OPG orders submitted after 9:28am but before 7:00pm ET will be rejected. OPG orders submitted after 7:00pm will be queued and routed to the following day’s opening auction. On open/on close orders are routed to the primary exchange. Such orders do not necessarily execute exactly at 9:30am / 4:00pm ET but execute per the exchange’s auction rules.
+    - cls: Use this TIF with a market/limit order type to submit “market on close” (MOC) and “limit on close” (LOC) orders. This order is eligible to execute only in the market closing auction. Any unfilled orders after the close will be cancelled. CLS orders submitted after 3:50pm but before 7:00pm ET will be rejected. CLS orders submitted after 7:00pm will be queued and routed to the following day’s closing auction. Only available with API v2.
+    - ioc: An Immediate Or Cancel (IOC) order requires all or part of the order to be executed immediately. Any unfilled portion of the order is canceled. Only available with API v2. Most market makers who receive IOC orders will attempt to fill the order on a principal basis only, and cancel any unfilled balance. On occasion, this can result in the entire order being cancelled if the market maker does not have any existing inventory of the security in question.
+    - fok: A Fill or Kill (FOK) order is only executed if the entire order quantity can be filled, otherwise the order is canceled. Only available with API v2.
     """
 
     DAY = "day"

b8fbf21a2e92fb9329a4a183ca43c63f3e96cd68
Chihiro Hio <chihiro.hio@alpaca.markets>
2024-01-25 10:47:34 +0900

    fix: remove unused import

--- alpaca/data/live/option.py
+++ alpaca/data/live/option.py
@@ -2,7 +2,6 @@ from typing import Dict, Optional
 
 from alpaca.common.enums import BaseURL
 from alpaca.common.websocket import BaseStream
-from alpaca.data.enums import DataFeed
 
 
 class OptionDataStream(BaseStream):

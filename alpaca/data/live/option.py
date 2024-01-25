40e503625999342d8d914cb804c3dd2ecd7bff49
hiohiohio <hiohiohio@users.noreply.github.com>
2024-01-25 10:53:28 +0900

    feat: Support option stream data (#407)
    * feat: support option stream data
    
    * fix: remove unused import

---
+++ alpaca/data/live/option.py
@@ -0,0 +1,43 @@
+from typing import Dict, Optional
+
+from alpaca.common.enums import BaseURL
+from alpaca.common.websocket import BaseStream
+
+
+class OptionDataStream(BaseStream):
+    """
+    A WebSocket client for streaming live option data.
+
+    See BaseStream for more information on implementation and the methods available.
+    """
+
+    def __init__(
+        self,
+        api_key: str,
+        secret_key: str,
+        raw_data: bool = False,
+        websocket_params: Optional[Dict] = None,
+        url_override: Optional[str] = None,
+    ) -> None:
+        """
+        Instantiates a WebSocket client for accessing live option data.
+
+        Args:
+            api_key (str): Alpaca API key.
+            secret_key (str): Alpaca API secret key.
+            raw_data (bool, optional): Whether to return wrapped data or raw API data. Defaults to False.
+            websocket_params (Optional[Dict], optional): Any parameters for configuring websocket connection. Defaults to None.
+            url_override (Optional[str]): If specified allows you to override the base url the client
+              points to for proxy/testing. Defaults to None.
+        """
+        super().__init__(
+            endpoint=(
+                url_override
+                if url_override is not None
+                else BaseURL.OPTION_DATA_STREAM.value + "/v1beta1/opra"
+            ),
+            api_key=api_key,
+            secret_key=secret_key,
+            raw_data=raw_data,
+            websocket_params=websocket_params,
+        )

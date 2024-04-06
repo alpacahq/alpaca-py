3e4aa195ec80150a4a1f56b028dae48303c350e9
Isaac Trevino <isaac.trevino@outlook.com>
2024-04-05 19:01:41 -0500

    initial changes

--- alpaca/data/historical/crypto.py
+++ alpaca/data/historical/crypto.py
@@ -16,6 +16,7 @@ from alpaca.data.models import BarSet, QuoteSet, TradeSet, Orderbook, Trade, Quo
 from alpaca.data.historical.stock import DataExtensionType
 from alpaca.data.requests import (
     CryptoBarsRequest,
+    CryptoQuoteRequest,
     CryptoTradesRequest,
     CryptoLatestTradeRequest,
     CryptoLatestQuoteRequest,
@@ -97,6 +98,35 @@ class CryptoHistoricalDataClient(RESTClient):
             return raw_bars
 
         return BarSet(raw_bars)
+
+    def get_crypto_quotes(
+        self, request_params: CryptoQuoteRequest, feed: CryptoFeed = CryptoFeed.US
+    ) -> Union[QuoteSet, RawData]:
+        """Returns the latest quote for a coin.
+
+        Args:
+            request_params (CryptoBarsRequest): The parameters for the request.
+            feed (CryptoFeed): The data feed for crypto quotes.
+
+        Returns:
+            Union[QuoteSet, RawData]: The crypto quote data either in raw or wrapped form
+        """
+
+        params = request_params.to_request_fields()
+
+        # paginated get request for market data api
+        raw_quotes = self._data_get(
+            endpoint_asset_class="crypto",
+            endpoint_data_type="quotes",
+            api_version="v1beta3",
+            feed=feed,
+            **params,
+        )
+
+        if self._use_raw_data:
+            return raw_quotes
+
+        return QuoteSet(raw_quotes)
 
     def get_crypto_trades(
         self, request_params: CryptoTradesRequest, feed: CryptoFeed = CryptoFeed.US

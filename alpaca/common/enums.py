e1a90974ee8b904173546fe6d3ab08146f594ca1
Emmanuel Derrick-Brown Akolbire <53496288+DeXtreme@users.noreply.github.com>
2024-06-28 14:56:36 +0000

    feat: Add sandbox data support (#464)
    * Add sandbox data url
    Adds the sandbox data endpoint url to the BaseURL enum
    
    * Add sandbox argument to clients
    Adds the sandbox parameter to the historical clients and sets the base_url to the
    sandbox url. Also adds the use_basic_auth argument to the CryptoHistoricalDataClient
    as it's required for broker api sandbox authentication

--- alpaca/common/enums.py
+++ alpaca/common/enums.py
@@ -9,6 +9,7 @@ class BaseURL(str, Enum):
     TRADING_PAPER = "https://paper-api.alpaca.markets"
     TRADING_LIVE = "https://api.alpaca.markets"
     DATA = "https://data.alpaca.markets"
+    DATA_SANDBOX = "https://data.sandbox.alpaca.markets"
     MARKET_DATA_STREAM = "wss://stream.data.alpaca.markets"
     OPTION_DATA_STREAM = "wss://stream.data.alpaca.markets"  # Deprecated: use MARKET_DATA_STREAM instead!
     TRADING_STREAM_PAPER = "wss://paper-api.alpaca.markets/stream"
